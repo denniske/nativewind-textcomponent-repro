cd12458ee3ac9cf7ac9cdcb66d3fe5bc6949d01d
Dennis Keil <dennis.keil10@gmail.com>
2025-09-18 23:35:39 +0200

    update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,20 +4,12 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 import '../global.css';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen name="index" options={{ title: 'Index' }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
